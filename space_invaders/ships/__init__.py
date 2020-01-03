c29ab533ec9fedde281d13762956eded407ada67
itskatt <raph10@mail.com>
2020-01-03 16:44:15 +0100

    remove this

--- space_invaders/ships/__init__.py
+++ space_invaders/ships/__init__.py
@@ -9,36 +9,6 @@ from ..lasers import AutoLaser, BasicLaser
 from .base import BaseFireingShip, BaseEnemiShip
 
 
-class ShipShare:  # TODO: remove
-    def __init__(self):
-        self._dict = {}
-
-    def add_ship(self, ship_type, perc):
-        if ship_type not in self._dict:
-            self.modify_ship(ship_type, perc)
-
-    def modify_ship(self, ship_type, perc):
-        self._dict[ship_type] = perc
-        self._ajust()
-
-    def remove_ship(self, ship_type):
-        del self._dict[ship_type]
-        self._ajust()
-
-    def get_shares(self, count):
-        d = {}
-        for k in self._dict.keys():
-            d[k] = round(count * self._dict[k] / 100)
-
-        return d.items()
-
-    def _ajust(self):
-        total = sum(self._dict.values())
-        if total != 100:
-            for k in self._dict.keys():
-                self._dict[k] = self._dict[k] * 100 / total
-
-
 class Ship(BaseFireingShip):
     def __init__(self, game):
         super().__init__(game)
